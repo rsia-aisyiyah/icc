71ac6139b22b9c1cfc71fb87459b51aa51ef9aaf
FAISAL HALIM <ffaisalhalim@gmail.com>
2024-05-29 09:55:18 +0700

    refactor: Update login form component
    This commit refactors the Login.vue component by updating the form configuration and handling the response. It also improves the layout and styling of the form elements. The changes include:
    - Updating the form endpoint to use the API_V2_URL from the runtime configuration
    - Changing the register_title element from h1 to h2
    - Changing the page_subtitle element to use the <p> tag
    - Updating the username and password TextElement components with new properties and styles
    - Changing the button-label of the register ButtonElement component to "Login"
    
    These changes aim to enhance the functionality and user experience of the login form.

--- components/Form/Login.vue
+++ components/Form/Login.vue
@@ -1,91 +1,68 @@
 <script setup>
-  const form$ = ref(null)
+const config = useRuntimeConfig()
+const form$ = ref(null)
 
-  onMounted(() => {
-    const config = useRuntimeConfig()
-    form$.value.update({
-      username: config.public.TEST_USERNAME,
-      password: config.public.TEST_PASSWORD
+onMounted(() => {
+  const config = useRuntimeConfig()
+  form$.value.update({
+    username: config.public.TEST_USERNAME,
+    password: config.public.TEST_PASSWORD
+  })
+});
+
+const handleResponse = (response, form$) => {
+  const toast = useToast()
+  const router = useRouter()
+  const tokenStore = useAccessTokenStore()
+
+  if (response.status == 200) {
+    tokenStore.set(response.data.access_token)
+    router.push('/')
+  } else {
+    toast.add({
+      title: 'Login failed',
+      description: response.data.message,
+      status: 'error',
+      color: 'red'
     })
-  });
-
-  const handleResponse = (response, form$) => {
-    const toast = useToast()
-    const router = useRouter()
-    const tokenStore = useAccessTokenStore()
-    
-    if (response.status == 200) {
-      tokenStore.set(response.data.access_token)
-      router.push('/')
-    } else {
-      toast.add({
-        title: 'Login failed',
-        description: response.data.message,
-        status: 'error',
-        color: 'red'
-      })
-    }
   }
+}
 </script>
 
 <template>
-  <Vueform  
-    ref="form$"
-    size="md"
-    :display-errors="false"
-    method="POST"
-    endpoint="http://172.24.19.22/rsia/api/v2/user/auth/login"
-    @response="handleResponse"
-  >
-    <StaticElement
-      name="register_title"
-      content="Login"
-      tag="h1"
-    />
-    <StaticElement
-      name="page_subtitle"
-      content="Login untuk dapat menggunakan layanan yang ada."
-    />
-    <StaticElement
-      name="divider"
-      tag="hr"
-    />
-    <TextElement
-      name="username"
-      label="Username ( NIK )"
+  <Vueform ref="form$" size="md" :display-errors="false" method="POST" :endpoint="`${config.public.API_V2_URL}/user/auth/login`" @response="handleResponse">
+    <StaticElement name="register_title" content="Login" tag="h2" />
+    <StaticElement name="page_subtitle" content="Login untuk dapat menggunakan layanan yang ada." tag="p" element="p" size="sm"/>
+    <StaticElement name="divider" tag="hr" />
+    
+    <TextElement 
+      name="username" 
+      label="Username ( NIK )" 
       info="NIK Karywan, sesuai dengan kartu karyawan anda. "
-      placeholder="username ( nik )"
+      placeholder="username ( nik )" 
+      field-name="username" 
+      autocomplete="off" 
+      :floating="false" 
+      :rules="[ 'required' ]" 
+      id="username" 
+      size="md"
+    />
+      
+    <TextElement 
+      name="password" 
+      label="Password" 
+      input-type="password" 
+      placeholder="password" 
+      field-name="password" 
       :floating="false"
-      field-name="username"
-      :rules="[
-        'required',
-      ]"
-      id="username"
-      autocomplete="off"
+      :rules="[ 'required' ]" 
+      autocomplete="off" 
+      id="password" 
+      size="md"
     />
-    <TextElement
-      name="password"
-      input-type="password"
-      label="Password"
-      placeholder="password"
-      :floating="false"
-      field-name="password"
-      :rules="[
-        'required',
-      ]"
-      autocomplete="off"
-      id="password"
-    />
-    <StaticElement
-      name="divider_1"
-      tag="hr"
-    />
-    <ButtonElement
-      name="register"
-      :submits="true"
-      button-label="Create account"
-      :full="true"
-      size="lg"
-    />
+    
+    <StaticElement name="divider_1" tag="hr" />
+    
+    <ButtonElement name="register" :submits="true" button-label="Login" :full="true" size="lg" />
   </Vueform>
 </template>
