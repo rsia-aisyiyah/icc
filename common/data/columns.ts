d2977105cb55664fa8a2b55c094d8bdaf76f1874
FAISAL HALIM <ffaisalhalim@gmail.com>
2024-05-31 09:37:47 +0700

    update : remove unused columns and add data SEP columns.

--- common/data/columns.ts
+++ common/data/columns.ts
@@ -7,6 +7,18 @@ export const pasienRanapColumns = [
   { label: "Tanggal Keluar", key: "tgl_keluar" },
   { label: "Real Cost", key: "real_cost" },
   { label: "Groupping tarif", key: "groupping_tarif" },
-  { label: "Mining tarif", key: "mining_tarif" },
+  // { label: "Mining tarif", key: "mining_tarif" },
   { label: "Action", key: "action" },
+];
+
+export const dataSepColumns = [
+  { label: "No SEP", key: "no_sep" },
+  { label: "No Rawat", key: "no_rawat" },
+  { label: "Pasien", key: "nama_pasien" },
+  { label: "Peserta", key: "peserta" },
+  { label: "No Kartu", key: "no_kartu" },
+  { label: "Diagnosa Awal", key: "diagawal" },
+  { label: "Tgl Masuk", key: "tglrujukan" },
+  { label: "Tgl Keluar", key: "tglpulang" },
+  { label: "Action", key: "action" }
 ];
