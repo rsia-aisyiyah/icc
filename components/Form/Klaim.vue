de6c61e48b32a551d7ea94b14c8c9ed285ea3a57
FAISAL HALIM <ffaisalhalim@gmail.com>
2024-07-02 13:36:54 +0700

    refactor: Update klaim/[sep]/index.vue component
    This commit updates the klaim/[sep]/index.vue component to improve its functionality and data retrieval. The changes include:
    
    - Updated the props passed to the FormKlaim component to include the bridgingSep and allData properties.
    - Added null checks for the bridgingSep and allData properties to handle potential errors.
    - Added additional data retrieval logic to fetch regPeriksa and kamarInap data using multiple fetch requests.
    - Updated the conditional rendering logic to display the FormKlaim component only when all data is available.
    
    These changes aim to enhance the functionality and user experience of the klaim/[sep]/index.vue component.

--- components/Form/Klaim.vue
+++ components/Form/Klaim.vue
@@ -1,42 +1,123 @@
 <script lang="ts" setup>
-  import type { SepData } from '~/types/sep'
+import type { RegPeriksa } from '~/types/regPeriksa';
+import type { KamarInap } from '~/types/kamarInap';
+import type { SepData } from '~/types/sep'
+import { parse, differenceInMinutes } from 'date-fns';
 
-  const form$ = ref({} as any)
-  const { sep } = defineProps<{
-    sep: SepData
-  }>();
+const form$ = ref({} as any)
+const tglRawat = ref({ masuk: null, keluar: null } as { masuk: string | null, keluar: string | null })
+const { sep, regPeriksa, kamarInap } = defineProps<{
+  sep?: SepData
+  regPeriksa?: RegPeriksa
+  kamarInap?: KamarInap
+}>();
 
-  onMounted(() => {
+const parseCaraPulang = (label: string) => {
+  switch (label.toLowerCase()) {
+    case 'atas persetujuan dokter':
+      return 'home'
+    case 'dirujuk':
+      return 'transfer'
+    case 'atas permintaan sendiri':
+      return 'refuse'
+    case 'meninggal':
+      return 'deceased'
+    case 'lain-lain':
+      return 'other'
+    default:
+      return ''
+  }
+}
+
+let getDiff = (masuk: string | null, keluar: string | null) => {
+  // Set timezone offset for 'Asia/Jakarta' (UTC+7)
+  let timeZoneOffset = 7 * 60; // 7 hours in minutes
+
+  if (!masuk || !keluar) {
+    return { days: 0, hours: 0, minutes: 0 }; // Handle case where masuk or keluar is null or undefined
+  }
+
+  // Convert masuk and keluar to Date with locale id-ID
+  let dateMasuk = parse(masuk, 'yyyy-MM-dd HH:mm:ss', new Date());
+  let dateKeluar = parse(keluar, 'yyyy-MM-dd HH:mm:ss', new Date());
+
+  // Adjust the dates to the 'Asia/Jakarta' timezone
+  let adjustedDateMasuk = new Date(dateMasuk.getTime() + timeZoneOffset * 60 * 1000);
+  let adjustedDateKeluar = new Date(dateKeluar.getTime() + timeZoneOffset * 60 * 1000);
+
+  // Calculate total difference in minutes
+  let totalMinutes = differenceInMinutes(adjustedDateKeluar, adjustedDateMasuk);
+
+  let diffDays = Math.floor(totalMinutes / (60 * 24)) + 1;
+  let diffHours = Math.floor(totalMinutes / 60);
+  let diffMinutes = (totalMinutes % 60);
+
+  return { days: diffDays, hours: diffHours, minutes: diffMinutes };
+}
+
+
+let updateLos = (masuk: any, keluar: any) => {
+  if (masuk && keluar) {
+    let { days, hours, minutes } = getDiff(masuk, keluar);
     form$.value.update({
-      jenis_rawat: sep.jnspelayanan,
+      los_hari: days,
+      los_jam: `${String(hours).padStart(2, '0')}:${String(minutes).padStart(2, '0')}`,
     })
-  })
+  }
+}
+
+onMounted(() => {
+  if (regPeriksa && kamarInap && kamarInap.detail.length > 0) {
+    form$.value.update({
+      cara_bayar: '00003',
+      no_sep: sep?.no_sep,
+      no_peserta: sep?.no_kartu,
+      jenis_rawat: sep?.jnspelayanan,
+      kelas_hak: sep?.klsrawat,
+      tgl_masuk: regPeriksa.tgl_registrasi + ' ' + regPeriksa.jam_reg,
+      tgl_keluar: kamarInap.detail[0].tgl_keluar + ' ' + kamarInap.detail[0].jam_keluar,
+
+      los_hari: kamarInap.lama_inap,
+      los_jam: kamarInap.lama_jam,
+
+      umur: regPeriksa.umurdaftar + ' ' + regPeriksa.sttsumur,
+      berat_lahir: regPeriksa.umurdaftar == 0 ? regPeriksa?.pasien_bayi?.berat_badan : 0,
+
+      cara_pulang: parseCaraPulang(kamarInap.detail[0].stts_pulang || ''),
+    })
+  }
+})
 </script>
+
 
 <template>
   <Vueform ref="form$" size="sm" :display-errors="false">
     <StaticElement name="h3" tag="h3" content="Klaim Form" description="Form Klaim INA-CBGs" />
     <GroupElement name="container_1">
       <GroupElement name="jaminan" :columns="{ container: 3 }">
-        <SelectElement name="select" :search="true" :native="false" label="Jaminan / Cara Bayar" input-type="search" autocomplete="off" items="http://localhost:3000/cara_bayar.json" />
+        <SelectElement name="cara_bayar" :search="true" :native="false" label="Jaminan / Cara Bayar" input-type="search"
+          autocomplete="off" items="http://localhost:3000/cara_bayar.json" />
       </GroupElement>
       <GroupElement name="column2" :columns="{ container: 3 }">
         <!-- console the value -->
-        <TextElement name="no_peserta" label="No. Peserta" v-on:change="(a: any) => console.log(a)" />
+        <TextElement name="no_peserta" label="No. Peserta" description="Readonly" :readonly="true"
+          v-on:change="(a: any) => console.log(a)" />
       </GroupElement>
       <GroupElement name="column3" :columns="{ container: 3 }">
-        <TextElement name="no_sep" label="No. SEP" />
+        <TextElement name="no_sep" label="No. SEP" description="Readonly" :readonly="true" />
       </GroupElement>
       <GroupElement name="column4" :columns="{ container: 3 }">
-        <SelectElement name="cob" :search="true" :native="false" label="COB" input-type="search" autocomplete="off" items="http://localhost:3000/cob.json" value-prop="value" label-prop="label" />
+        <SelectElement name="cob" :search="true" :native="false" label="COB" input-type="search" autocomplete="off"
+          items="http://localhost:3000/cob.json" value-prop="value" label-prop="label" />
       </GroupElement>
     </GroupElement>
     <GroupElement name="container_2" class="mt-4">
       <GroupElement name="column1" :columns="{ container: 3 }">
-        <RadiogroupElement name="jenis_rawat" id="jenis_rawat" field-name="jenis_rawat" view="tabs" label="Jenis Rawat" :items="[
-          { value: '2', label: 'Rawat Jalan' },
-          { value: '1', label: 'Rawat Inap' },
-        ]" />
+        <RadiogroupElement name="jenis_rawat" id="jenis_rawat" field-name="jenis_rawat" view="tabs" label="Jenis Rawat"
+          :items="[
+            { value: '2', label: 'Rawat Jalan' },
+            { value: '1', label: 'Rawat Inap' },
+          ]" />
       </GroupElement>
       <GroupElement name="column2" :columns="{ container: 6 }">
         <CheckboxgroupElement name="jenis_rawat_1" view="tabs" label="Jenis Rawat" :items="[
@@ -44,17 +125,17 @@
           { value: '1', label: 'Ada Rawat Intensif' },
         ]" :conditions="[
           ['container_2.column1.jenis_rawat', 'in', ['1',],],
-          ['container_1.jaminan.select', 'not_in', ['00076',],],
+          ['container_1.jaminan.cara_bayar', 'not_in', ['JPS',],],
         ]" />
         <CheckboxgroupElement name="jenis_rawat_3" view="tabs" label="Jenis Rawat" :items="[
           { value: '1', label: 'Ada Rawat Intensif' },
         ]" :conditions="[
           ['container_2.column1.jenis_rawat', 'in', ['1',],],
-          ['container_1.jaminan.select', 'in', ['00076',],],
+          ['container_1.jaminan.cara_bayar', 'in', ['JPS',],],
         ]" :columns="{ container: 6 }" />
         <CheckboxgroupElement name="jenis_rawat_2" view="tabs" label="Jenis Rawat" :conditions="[
           ['container_2.column1.jenis_rawat', 'in', ['2',],],
-          ['container_1.jaminan.select', 'not_in', ['00076',],],
+          ['container_1.jaminan.cara_bayar', 'not_in', ['JPS',],],
         ]" :items="[
           { value: '0', label: 'Kelas Ekslusif' },
         ]" :columns="{ container: 6 }" />
@@ -71,18 +152,21 @@
     </GroupElement>
     <GroupElement name="container_3" class="mt-3" label="<b>Tanggal Rawat</b>">
       <GroupElement name="column1" :columns="{ container: 4 }">
-        <DateElement name="tgl_masuk" label="Masuk" :time="true" default="2024-05-21 12:00" min="2024-05-08 00:00"
-          :addons="{ before: '📆' }" />
+        <DateElement name="tgl_masuk" label="Masuk" :time="true" :seconds="true" display-format="DD MMM YYYY HH:mm:ss"
+          :addons="{ before: '📆' }"
+          v-on:change="(masuk: any) => { tglRawat.masuk = masuk; updateLos(masuk, tglRawat.keluar) }" />
       </GroupElement>
       <GroupElement name="column2" :columns="{ container: 4 }">
-        <DateElement name="tgl_keluar" label="Keluar" :time="true" default="2024-05-21 12:00" :conditions="[
-          ['container_2.column1.jenis_rawat', 'in', ['1',],],
-        ]" :addons="{ before: '📆' }" />
+        <DateElement name="tgl_keluar" label="Keluar" :time="true" :seconds="true" display-format="DD MMM YYYY HH:mm:ss"
+          :conditions="[['container_2.column1.jenis_rawat', 'in', ['1',],],]" :addons="{ before: '📆' }"
+          v-on:change="(keluar: any) => { tglRawat.keluar = keluar; updateLos(tglRawat.masuk, keluar) }" />
       </GroupElement>
       <GroupElement name="column3" :columns="{ container: 4 }">
-        <TextElement name="umur" input-type="number" :rules="[ 'nullable', 'min:0', 'numeric' ]" autocomplete="off" label="Umur" :readonly="true" info="Umur Dalam bulan" />
+        <TextElement name="umur" autocomplete="off" label="Umur" description="Readonly" :readonly="true" />
       </GroupElement>
-      <SelectElement name="cara_masuk" items="http://localhost:3000/cara_masuk.json" :search="true" :native="false" label="Cara Masuk" input-type="search" autocomplete="off" :default="0" :columns="{ container: 6 }" />
+      <SelectElement name="cara_masuk" items="http://localhost:3000/cara_masuk.json" :search="true" :native="false"
+        class="mt-3" label="Cara Masuk" input-type="search" autocomplete="off" :default="0"
+        :columns="{ container: 6 }" />
     </GroupElement>
     <StaticElement name="divider_1" tag="hr" top="1" bottom="1" />
     <RadiogroupElement name="kelas_pelayanan" view="tabs" label="Kelas Pelayanan" :items="[
@@ -121,27 +205,22 @@
           default: { container: 4 },
           lg: { container: 6 },
         }">
-          <TextElement name="los_hari" input-type="number" :rules="[
-            'nullable',
-            'min:0',
-            'numeric',
-          ]" autocomplete="off" label="HARI" info="LOS Dalam hari" :addons="{ after: 'Hari' }" />
+          <TextElement name="los_hari" input-type="number" :rules="['nullable', 'min:0', 'numeric']" autocomplete="off"
+            label="HARI" info="LOS Dalam hari" description="Readonly" :readonly="true" :addons="{ after: 'Hari' }" />
         </GroupElement>
         <GroupElement name="column2" :columns="{
           default: { container: 4 },
           lg: { container: 3 },
         }">
-          <TextElement name="los_jam" label="JAM" input-type="time" />
+          <TextElement name="los_jam" label="JAM" description="Readonly" :readonly="true" :addons="{ after: 'Jam' }" />
         </GroupElement>
         <GroupElement name="column2_1" :columns="{
           default: { container: 4 },
           lg: { container: 3 },
         }">
-          <TextElement name="berat_lahir" input-type="number" :rules="[
-            'nullable',
-            'min:0',
-            'numeric',
-          ]" autocomplete="off" label="Berat lahir" info="Berat lahir dalam gram" :addons="{ after: 'gram' }" />
+          <TextElement name="berat_lahir" input-type="number" :rules="['nullable', 'min:0', 'numeric']"
+            autocomplete="off" label="Berat lahir" info="Berat lahir dalam gram" description="Readonly" :readonly="true"
+            :addons="{ after: 'gram' }" />
         </GroupElement>
       </GroupElement>
       <GroupElement name="container_4_2">
@@ -153,23 +232,26 @@
           <TextElement name="chronic" label="Chronic" />
         </GroupElement>
         <GroupElement name="column3" :columns="{ container: 4 }">
-          <SelectElement name="cara_pulang" items="http://localhost:3000/cara_pulang.json" :search="true" :native="false" label="Cara Pulang" input-type="search" autocomplete="off" />
+          <SelectElement name="cara_pulang" items="http://localhost:3000/cara_pulang.json" :search="true"
+            :native="false" label="Cara Pulang" input-type="search" autocomplete="off" />
         </GroupElement>
       </GroupElement>
       <GroupElement class="mt-3" name="container_4_3">
         <GroupElement name="column1" :columns="{ container: 6 }">
-          <SelectElement name="dpjp" items="http://localhost:3000/dpjp.json" :search="true" :native="false" label="DPJP" input-type="search" autocomplete="off" />
+          <SelectElement name="dpjp" items="http://localhost:3000/dpjp.json" :search="true" :native="false" label="DPJP"
+            input-type="search" autocomplete="off" />
         </GroupElement>
         <GroupElement name="column2" :columns="{ container: 6 }">
-          <SelectElement name="jenis_tarif" items="http://localhost:3000/jenis_tarif.json" :search="true" :native="false" label="Jenis Tarif" input-type="search" autocomplete="off" />
+          <SelectElement name="jenis_tarif" items="http://localhost:3000/jenis_tarif.json" :search="true"
+            :native="false" label="Jenis Tarif" input-type="search" autocomplete="off" />
         </GroupElement>
       </GroupElement>
     </GroupElement>
     <StaticElement name="divider" tag="hr" bottom="1" top="1" :conditions="[
-      ['container_1.jaminan.select', 'not_in', ['00076',],],
+      ['container_1.jaminan.cara_bayar', 'not_in', ['JPS',],],
     ]" />
     <GroupElement name="container2" :conditions="[
-      ['container_1.jaminan.select', 'not_in', ['00076',],],
+      ['container_1.jaminan.cara_bayar', 'not_in', ['JPS',],],
     ]">
       <GroupElement name="column1" :columns="{ container: 2 }">
         <RadiogroupElement name="pasien_tb" view="tabs" label="Pasien TB" :items="[
@@ -191,7 +273,7 @@
     </GroupElement>
     <GroupElement class="mt-4" name="container2_1" :conditions="[
       ['container_2.column1.jenis_rawat', 'in', ['1',],],
-      ['container_1.jaminan.select', 'not_in', ['00076',],
+      ['container_1.jaminan.cara_bayar', 'not_in', ['JPS',],
       ],
     ]">
       <GroupElement name="column1_1" :columns="{ container: 2 }">
@@ -215,7 +297,8 @@
     <GroupElement name="container" :conditions="[
       ['container_2.column2.jenis_rawat_2', 'in', ['0',],],
     ]">
-      <TextElement name="Tarif Poli Eks" input-type="number" :rules="['nullable', 'min:0', 'numeric',]" autocomplete="off" label="Tarif Poli Eks." :addons="{ before: 'Rp' }" />
+      <TextElement name="Tarif Poli Eks" input-type="number" :rules="['nullable', 'min:0', 'numeric',]"
+        autocomplete="off" label="Tarif Poli Eks." :addons="{ before: 'Rp' }" />
     </GroupElement>
     <StaticElement name="divider_2" tag="hr" bottom="1" top="1" />
     <StaticElement name="h3_1" tag="h3" content="Tarif Rumah Sakit" />
@@ -307,10 +390,10 @@
       </GroupElement>
     </GroupElement>
     <StaticElement name="divider_7" tag="hr" top="1" bottom="1" :conditions="[
-      ['container_1.jaminan.select', 'in', ['00076',],],
+      ['container_1.jaminan.cara_bayar', 'in', ['JPS',],],
     ]" />
     <GroupElement name="container_6" :conditions="[
-      ['container_1.jaminan.select', 'in', ['00076',],],
+      ['container_1.jaminan.cara_bayar', 'in', ['JPS',],],
     ]">
       <MultifileElement name=" Resume Medis" label="Resume Medis" :urls="{}" />
       <MultifileElement name="Bukti Penerimaan Layanan" label="Bukti Penerimaan Layanan" :urls="{}" />
@@ -339,21 +422,24 @@
       <GroupElement name="ina_container" :conditions="[
         ['unu_ina', 'in', ['INA',],],
       ]">
-        <StaticElement name="h4_1" tag="h4" content="INA Grouper" description="Sistem pengelompokan baru untuk diagnosis dan penyakit yang disesuaikan dengan kondisi di Indonesia." />
+        <StaticElement name="h4_1" tag="h4" content="INA Grouper"
+          description="Sistem pengelompokan baru untuk diagnosis dan penyakit yang disesuaikan dengan kondisi di Indonesia." />
       </GroupElement>
     </GroupElement>
     <StaticElement name="divider_5" tag="hr" bottom="1" top="1" />
     <StaticElement name="h3_3" tag="h3" content="Data Klinis" description="Tekanan Darah (mmHg):" />
     <GroupElement name="container2_4">
       <GroupElement name="column1" :columns="{ container: 6 }">
-        <TextElement name="Sistole" input-type="number" :rules="['nullable','numeric' ]" autocomplete="off" label="Sistole" default="0" />
+        <TextElement name="Sistole" input-type="number" :rules="['nullable', 'numeric']" autocomplete="off"
+          label="Sistole" default="0" />
       </GroupElement>
       <GroupElement name="column2" :columns="{ container: 6 }">
-        <TextElement name="Diastole" input-type="number" :rules="['nullable','numeric' ]" autocomplete="off" label="Diastole" default="0" />
+        <TextElement name="Diastole" input-type="number" :rules="['nullable', 'numeric']" autocomplete="off"
+          label="Diastole" default="0" />
       </GroupElement>
     </GroupElement>
     <GroupElement name="container_7" :conditions="[
-      ['container_1.jaminan.select', 'in', ['00076',],],
+      ['container_1.jaminan.cara_bayar', 'in', ['JPS',],],
     ]">
       <StaticElement name="divider_8" tag="hr" bottom="1" />
       <GroupElement name="container2_6">
--- pages/klaim/[sep]/index.vue
+++ pages/klaim/[sep]/index.vue
@@ -2,14 +2,18 @@
   <UContainer>
     <UCard>
       <FormKlaim 
-        :sep="bridgingSep.data"
+        :sep="bridgingSep?.data"
+        :regPeriksa="allData.regPeriksa?.data"
+        :kamarInap="allData.kamarInap?.data"
       />
     </UCard>
   </UContainer>
 </template>
 
 <script lang="ts" setup>
-import type { ResponseSepData } from '~/types/sep'
+import type { ResponseSepData, SepData } from '~/types/sep'
+import type { ResponseRegPeriksa, RegPeriksa } from '~/types/regPeriksa'
+import type { KamarInapResponse, KamarInap } from '~/types/kamarInap';
 
 const route = useRoute();
 const config = useRuntimeConfig()
@@ -17,8 +21,46 @@ const tokenStore = useAccessTokenStore()
 
 const no_sep = ref(route.params.sep);
 
-const { data: bridgingSep, pending, error, refresh } = await useFetch<ResponseSepData>(`${config.public.API_V2_URL}/sep/${no_sep.value}`, {
+const allData = ref<{ regPeriksa: ResponseRegPeriksa | null, kamarInap: KamarInapResponse | null }>({
+  regPeriksa: null,
+  kamarInap: null
+})
+
+const { data: bridgingSep, pending: bridgingSepPending, error: bridgingSepError, refresh: bridgingSepRefresh } = await useFetch<ResponseSepData>(`${config.public.API_V2_URL}/sep/${no_sep.value}`, {
   method: 'GET',
   headers: { Authorization: `Bearer ${tokenStore.accessToken}` },
 })
+
+if (bridgingSepError.value) {
+  console.error(bridgingSepError.value)
+}
+
+// if bridgingSepPending.value = false && bridgingSepError.value = null then fetch regPeriksa using multiple fetch
+if (!bridgingSepPending.value && !bridgingSepError.value) {
+  console.log("Fetching detail Data");
+  const noRawat = bridgingSep?.value?.data.no_rawat || ''
+
+  const { data, pending, error, refresh, status } = await useAsyncData(`data-klaim-pasien-${btoa(noRawat)}`, async () => {
+    const [regPeriksa, kamarInap] = await Promise.all([
+      // Fetch Reg Periksa
+      $fetch<ResponseRegPeriksa>(`${config.public.API_V2_URL}/registrasi/periksa/${btoa(noRawat)}`, {
+        query: { include: 'pasienBayi' },
+        headers: { Authorization: `Bearer ${tokenStore.accessToken}` }
+      }),
+
+      // Fetch Kamar Inap
+      $fetch<KamarInapResponse>(`${config.public.API_V2_URL}/kamar/inap/${btoa(noRawat)}`, {
+        headers: { Authorization: `Bearer ${tokenStore.accessToken}` }
+      }),
+    ])
+
+    return { regPeriksa, kamarInap }
+  })  
+
+  if (error.value) {
+    console.error(error.value)
+  }
+
+  allData.value = data.value as any;
+}
 </script>
