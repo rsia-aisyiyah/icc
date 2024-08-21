41d020d48d1f1b47420b30e84a57ffaf5f6ae0b9
FAISAL HALIM <ffaisalhalim@gmail.com>
2024-08-21 09:37:05 +0700

    :sparkles: feat: Add ThemesSwitcher component
    This commit adds the ThemesSwitcher component, which allows users to switch between different color themes. The component includes a dropdown menu with options for dark, light, and system themes. It also includes icons representing each theme option. This addition enhances the user experience by providing a convenient way to change the color theme of the application.

---
+++ components/ThemesSwitcher.vue
@@ -0,0 +1,34 @@
+<!-- floating -->
+<template>
+  <div class="fixed bottom-4 left-4 z-50">
+    <div class="flex items-center space-x-2">
+      <UDropdown :items="items" v-model="selected" mode="hover" class="relative">
+        <!-- circle button toggle -->
+        <div class="w-10 h-10 flex items-center justify-center rounded-full bg-cool-500 border-[3px] border-cool-400 shadow-lg">
+          <UIcon :name="selected.icon" class="text-white text-xl"/>
+        </div> 
+
+        <template #item="{ item }">
+          <span class="truncate">{{ item.label }}</span>
+          <UIcon :name="item.icon" class="flex-shrink-0 h-4 w-4 text-gray-400 dark:text-gray-500 ms-auto" />
+        </template>
+      </UDropdown>
+    </div>
+  </div>
+</template>
+
+<script lang="ts" setup>
+const selected = ref({ label: "System", icon: "i-tabler-device-mobile", value: "system" })
+
+const toggleColorMode = (mode: string) => {
+  useColorMode().preference = mode
+  selected.value = items[0].find((item) => item.value === mode) || items[0][0]
+}
+
+const items = [[
+  { label: "Dark", icon: "i-tabler-moon", value: "dark", click: () => toggleColorMode("dark") },
+  { label: "Light", icon: "i-tabler-sun", value: "light", click: () => toggleColorMode("light") },
+  { label: "System", icon: "i-tabler-device-mobile", value: "system", click: () => toggleColorMode("system") }
+]];
+
+</script>
