41b71a86c0d12aaba193e5448f7ad27436dc1041
FAISAL HALIM <ffaisalhalim@gmail.com>
2024-10-17 12:36:27 +0700

    :hammer: update (helper): Add labelToValue utility function for fetching cara_pulang.json data

---
+++ utils/labelToValue.ts
@@ -0,0 +1,7 @@
+// get value from cara_pulang.json on public with passing label 
+export async function getCaraPulangByLabel(label: string) {
+  const data = await fetch('/cara_pulang.json').then((res) => res.json());
+
+  const result = data.find((item: any) => item.label === label);
+  return result ? result : { value: '', label: '' };
+} 
