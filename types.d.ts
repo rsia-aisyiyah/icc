fed4caec4624c47eb41bfa409d93ee61c16b35ff
FAISAL HALIM <ffaisalhalim@gmail.com>
2024-10-21 16:27:04 +0700

    :sparkles: feat: Add FormDataSchema for form data validation

--- types.d.ts
+++ types.d.ts
@@ -181,18 +181,18 @@ export type DiagnosaData = {
 // ==============================
 // ==================== REAL COST RAWAT INAP 
 export type RealCostRawatInap = {
-  detail_pemberian_obat: number;
-  kamar_inap: number;
-  operasi: number;
-  periksa_lab: number;
-  periksa_radiologi: number;
-  rawat_inap_dr_pr: number;
-  rawat_inap_pr: number;
-  rawat_inap_dr: number;
-  rawat_jalan_dr_pr: number;
-  rawat_jalan_pr: number;
-  rawat_jalan_dr: number;
-  total: number;
+  detail_pemberian_obat?: number;
+  kamar_inap?: number;
+  operasi?: number;
+  periksa_lab?: number;
+  periksa_radiologi?: number;
+  rawat_inap_dr_pr?: number;
+  rawat_inap_pr?: number;
+  rawat_inap_dr?: number;
+  rawat_jalan_dr_pr?: number;
+  rawat_jalan_pr?: number;
+  rawat_jalan_dr?: number;
+  total?: number;
   gabung?: Partial<RealCostRawatInap>;
 };
 
@@ -273,3 +273,9 @@ type ResourcePaginationMetaLink = {
   label: string;
   active: boolean;
 };
+
+// ==============================
+
+import { FormDataSchema } from '~/common/schema/formData'
+
+export type FormData = z.infer<typeof FormDataSchema>
