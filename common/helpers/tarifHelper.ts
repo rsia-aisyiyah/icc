a3112b77f5f8c60d68c92c17311c770b73058331
FAISAL HALIM <ffaisalhalim@gmail.com>
2024-10-21 16:26:54 +0700

    :sparkles: feat: Add getTotalTarifRS function to calculate total tarif in tarifHelper.ts

---
+++ common/helpers/tarifHelper.ts
@@ -0,0 +1,30 @@
+import type { FormData } from "~/common/types/formData";
+
+export const getTotalTarifRS = (state: FormData) => {
+  const nominalKey = [
+    "prosedur_non_bedah",
+    "prosedur_bedah",
+    "konsultasi",
+    "tenaga_ahli",
+    "keperawatan",
+    "penunjang",
+    "radiologi",
+    "laboratorium",
+    "pelayanan_darah",
+    "rehabilitasi",
+    "kamar",
+    "rawat_intensif",
+    "obat",
+    "obat_kronis",
+    "obat_kemoterapi",
+    "alkes",
+    "bmhp",
+    "sewa_alat"
+  ];
+
+  // get nominal value from state and sum it
+  return nominalKey.reduce((acc, key) => {
+    const value = state[key];
+    return acc + (value !== undefined && value !== '' ? Number(value.replace(/\.|,/g, '')) : 0);
+  }, 0);
+}
