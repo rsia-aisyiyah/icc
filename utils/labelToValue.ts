61d9779df762487213cc9ffaf1999760189878e0
FAISAL HALIM <ffaisalhalim@gmail.com>
2024-10-22 15:49:26 +0700

    :recycle: refactor: Update labelToValue.ts to return null instead of empty string when no result is found

--- utils/labelToValue.ts
+++ utils/labelToValue.ts
@@ -3,5 +3,5 @@ export async function getCaraPulangByLabel(label: string) {
   const data = await fetch('/cara_pulang.json').then((res) => res.json());
 
   const result = data.find((item: any) => item.label === label);
-  return result ? result : { value: '', label: '' };
+  return result ? result : { value: null, label: null };
 } 
