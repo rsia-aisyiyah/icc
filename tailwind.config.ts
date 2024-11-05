ef439fb78290b4c2db470500f53b6efcb6b84eae
FAISAL HALIM <ffaisalhalim@gmail.com>
2024-11-05 14:16:22 +0700

    :hammer: update: expand Tailwind CSS configuration to include additional file types and directories for improved styling coverage

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -5,6 +5,21 @@ module.exports = {
     './pages/**/*.vue',
     './layouts/**/*.vue',
     './components/**/*.vue',
+    './plugins/**/*.ts',
+    './utils/**/*.ts',
+    './data/**/*.ts',
+    './common/**/*.ts',
+
+    "./components/**/*.{vue,js,jsx,mjs,ts,tsx}",
+    "./layouts/**/*.{vue,js,jsx,mjs,ts,tsx}",
+    "./pages/**/*.{vue,js,jsx,mjs,ts,tsx}",
+    "./plugins/**/*.{js,ts,mjs}",
+    "./composables/**/*.{js,ts,mjs}",
+    "./utils/**/*.{js,ts,mjs}",
+    "./{A,a}pp.{vue,js,jsx,mjs,ts,tsx}",
+    "./{E,e}rror.{vue,js,jsx,mjs,ts,tsx}",
+    "./app.config.{js,ts,mjs}",
+    "./app/spa-loading-template.html",
 
 
     './vueform.config.ts', // or where `vueform.config.js` is located. Change `.js` to `.ts` if required.
