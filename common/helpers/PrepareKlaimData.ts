6676499c21a53c40d4a65307f0bdb24e9a75c6a6
FAISAL HALIM <ffaisalhalim@gmail.com>
2024-12-14 11:23:46 +0700

    :hammer: update (KlaimNew): enhance date formatting logic to include seconds in the output

--- common/helpers/PrepareKlaimData.ts
+++ common/helpers/PrepareKlaimData.ts
@@ -25,6 +25,7 @@ export const prepareKlaimData = (data: object) => {
               day: '2-digit',
               hour: '2-digit',
               minute: '2-digit',
+              second: '2-digit',
           })
           .replace(/\./g, ':'); // Ganti semua titik dengan tanda titik dua
 
