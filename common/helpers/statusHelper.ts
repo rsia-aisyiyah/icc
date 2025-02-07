453321c6857602be7c2af1b60c3dd1f33dbc3fdf
FAISAL HALIM <ffaisalhalim@gmail.com>
2025-02-07 10:31:42 +0700

    :recycle: refactor : change variant of 'pengajuan' status to 'outline' for improved UI consistency

--- common/helpers/statusHelper.ts
+++ common/helpers/statusHelper.ts
@@ -5,7 +5,7 @@ const setStatus = [
 
   { id: "verifikasi resume", label: "verifikasi resume", icon: "i-tabler-checklist", color: "blue", variant: "soft" },
   { id: "lengkap", label: "lengkap", icon: "i-tabler-checkbox", color: "lime", variant: "soft" },
-  { id: "pengajuan", label: "pengajuan", icon: "i-tabler-cube-send", color: "amber", variant: "soft" },
+  { id: "pengajuan", label: "pengajuan", icon: "i-tabler-cube-send", color: "amber", variant: "outline" },
   { id: "perbaiki", label: "perbaiki", icon: "i-tabler-hammer", color: "orange", variant: "soft" },
   { id: "disetujui", label: "disetujui", icon: "i-tabler-rosette-discount-check", color: "primary", variant: "soft" },
   { id: "klaim ambulans", label: "klaim ambulans", icon: "i-tabler-ambulance", color: "fuchsia", variant: "soft" },
